d1adaa4666f4128a2a4eb8e0b20a7ad350e4c643
Meekiavelique <meekiavelique@gmail.com>
2026-03-14 16:20:04 +0100

    feat(client): register resource reload listener

---
+++ src/main/java/com/meekdev/amnetic/client/AmneticClient.java
@@ -0,0 +1,29 @@
+package com.meekdev.amnetic.client;
+
+import com.meekdev.amnetic.client.post.internal.PostEffectRegistry;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+
+public class AmneticClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(
+                new SimpleSynchronousResourceReloadListener() {
+                    @Override
+                    public Identifier getFabricId() {
+                        return Identifier.of("amnetic", "post_effect_cache");
+                    }
+
+                    @Override
+                    public void reload(ResourceManager manager) {
+                        PostEffectRegistry.INSTANCE.invalidatePipelineCaches();
+                    }
+                }
+        );
+    }
+}
