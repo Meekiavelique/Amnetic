a2fcc0ec6cbd195b56b3cfea124112840070fb2a
Meekiavelique <meekiavelique@gmail.com>
2026-03-14 16:17:16 +0100

    chore: init fabric mod template

---
+++ src/main/java/com/meekdev/amnetic/Amnetic.java
@@ -0,0 +1,10 @@
+package com.meekdev.amnetic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Amnetic implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
